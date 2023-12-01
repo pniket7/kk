17300a27a945845b37e6aeb86e743d4faa465e7f
Niket Patil <pniket7@gmail.com>
2023-12-01 15:03:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,157 @@
 import openai
 import streamlit as st
-from utils import ChatSession
+import pickle
+import time
+import numpy as np
+import pandas as pd
+from typing import Optional, Union
+import re
+
+# List of predefined financial categories/terms
+financial_categories = ['loan', 'savings', 'investment', 'mortgage', 'insurance', 'credit card']
+
+def ErrorHandler(f, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        while True:
+            try:
+                f(*args, **kwargs)
+                break
+            # RateLimitError
+            except openai.error.RateLimitError:
+                print('Rate limit exceeded. I will be back shortly, please wait for a minute.')
+                time.sleep(60)
+            # AuthenticationError
+            except openai.error.AuthenticationError as e:
+                print(e)
+                raise
+    return wrapper
+
+class ChatSession:
+    completions = {
+        1: dict(
+            completion=openai.ChatCompletion,
+            model="gpt-3.5-turbo",
+            text='message.content',
+            prompt='messages'
+        ),
+        0: dict(
+            completion=openai.Completion,
+            model="text-davinci-003",
+            text='text',
+            prompt='prompt'
+        )
+    }
+
+    def __init__(self, gpt_name='GPT') -> None:
+        # History of all messages in the chat.
+        self.messages = []
+        # History of completions by the model.
+        self.history = []
+        # The name of the model.
+        self.gpt_name = gpt_name
+
+    def chat(self, user_input: Optional[Union[dict, str]] = None, verbose=True, *args, **kwargs):
+        """ Say something to the model and get a reply. """
+        completion_index = 0 if kwargs.get('logprobs', False) or kwargs.get('model') == 'text-davinci-003' else 1
+        completion = self.completions[completion_index]
+        user_input = self.__get_input(user_input=user_input, log=True)
+        user_input = self.messages if completion_index else self.messages[-1]['content']
+        kwargs.update({completion['prompt']: user_input, 'model': completion['model']})
+        if completion_index == 1:
+            kwargs.update({'temperature': 0.5})
+        self.__get_reply(completion=completion['completion'], log=True, *args, **kwargs)
+        self.history[-1].update({'completion_index': completion_index})
+        if verbose:
+            self._call_(1)
+
+    def display_probas(self, reply_index):
+        """ Display probabilities of each word for the given reply by the model. """
+
+        history = self.history[reply_index]
+        assert not history.completion_index
+        probas = history.logprobs.top_logprobs
+        return pd.concat([
+                pd.DataFrame(data=np.concatenate([[list(k.keys()), np.exp2(list(k.values())).round(2)]]).T,
+                             columns=[str(i), f'{i}_proba'],
+                             index=[f'candidate_{j}' for j in range(len(probas[0]))]
+                            ) for i, k in enumerate(probas)], axis=1).T
+
+    def inject(self, line, role):
+        """ Inject lines into the chat. """
+
+        self.__log(message={"role": role, "content": line})
+
+    def clear(self, k=None):
+        """ Clears session. If provided, last k messages are cleared. """
+        if k:
+            self.messages = self.messages[:-k]
+            self.history = self.history[:-k]
+        else:
+            self._init_()
+
+    def save(self, filename):
+        """ Saves the session to a file. """
+
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    def load(self, filename):
+        """ Loads up the session. """
+
+        with open(filename, 'rb') as f:
+            temp = pickle.load(f)
+            self.messages = temp.messages
+            self.history = temp.history
+
+    def merge(self, filename):
+        """ Merges another session from a file with this one. """
+
+        with open(filename, 'rb') as f:
+            temp = pickle.load(f)
+            self.messages += temp.messages
+            self.history += temp.history
+
+    def __get_input(self, user_input, log: bool = False):
+        """ Converts user input to the desired format. """
+
+        if user_input is None:
+            user_input = input("> ")
+        if not isinstance(user_input, dict):
+            user_input = {"role": 'user', "content": user_input}
+        if log:
+            self.__log(user_input)
+        return user_input
+
+    @ErrorHandler
+    def __get_reply(self, completion, log: bool = False, *args, **kwargs):
+        """ Calls the model. """
+        reply = completion.create(*args, **kwargs).choices[0]
+        if log:
+            if hasattr(reply, 'message'):
+                self.__log(message=reply.message, history=reply)
+            else:
+                self.__log(message={"role": 'assistant', "content": reply.text}, history=reply)
+        return reply
+
+    def __log(self, message: dict, history=None):
+        self.messages.append(message)
+        if history is not None:
+            assert isinstance(history, dict)
+            self.history.append(history)
+
+    def _call_(self, k: Optional[int] = None):
+        """ Display the full chat log or the last k messages. """
+
+        k = len(self.messages) if k is None else k
+        for msg in self.messages[-k:]:
+            message = msg['content']
+            who = {'user': 'User: ', 'assistant': f'{self.gpt_name}: '}[msg['role']]
+            print(who + message.strip() + '\n')
 
 def initialize_sessionAdvisor():
     advisor = ChatSession(gpt_name='Advisor')
     advisor.inject(
-        line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. Be brief in your responses.",
+        line="You are a financial advisor at a bank. Engage the user as a financial advisor, initiating the conversation by asking about their financial goals. Proceed with follow-up questions based solely on the user's immediate response, maintaining a strictly sequential flow. Ask one question at a time, waiting for and responding to each user input individually. Ensure that each response from the advisor contains only a single query or request for information, refraining from posing multiple questions or requests within the same reply. If the user mentions a specific financial goal or issue, acknowledge it, offer assistance, and continue with a single follow-up question. Be attentive to the user's needs and goals, keeping responses concise and focused.",
         role="user"
     )
     advisor.inject(line="Ok.", role="assistant")
@@ -25,18 +171,20 @@ def main():
     if "sessionAdvisor" not in st.session_state or st.session_state.sessionAdvisor is None:
         st.session_state.sessionAdvisor = initialize_sessionAdvisor()
 
-    # Display chat messages from history on app rerun
+    # Function to update chat history display
+    def update_chat_display(messages):
+        chat_messages = ""
+        if messages:
+            for message in messages:
+                role_color = "#0084ff" if message["role"] == "user" else "#9400D3"
+                alignment = "right" if message["role"] == "user" else "left"
+                chat_messages += f'<div style="text-align: {alignment}; margin-bottom: 10px;"><span style="background-color: {role_color}; color: white; padding: 8px 12px; border-radius: 20px; display: inline-block; max-width: 70%;">{message["content"]}</span></div>'
+        return chat_messages
+
+    # Display the chat history and bot thinking message together
     chat_container = st.empty()
-
-    # Display the chat history
-    chat_messages = ""
-    if st.session_state.chat_history:
-        for message in st.session_state.chat_history:
-            role_color = "#9400D3" if message["role"] == "user" else "#0084ff"
-            alignment = "left" if message["role"] == "user" else "right"
-            chat_messages += f'<div style="text-align: {alignment}; margin-bottom: 10px;"><span style="background-color: {role_color}; color: white; padding: 8px 12px; border-radius: 20px; display: inline-block; max-width: 70%;">{message["content"]}</span></div>'
-
-    chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll;">{chat_messages}</div>', unsafe_allow_html=True)
+    chat_and_thinking_display = update_chat_display(st.session_state.chat_history) + '<div id="thinking"></div>'
+    chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll; position: relative;">{chat_and_thinking_display}</div>', unsafe_allow_html=True)
 
     # Accept user input
     user_input = st.text_input("Type your message here...")
@@ -46,27 +194,35 @@ def main():
         # Add the user's message to the chat history
         st.session_state.chat_history.append({"role": "user", "content": user_input})
 
-        # Update the chat session with the user's input
-        st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
+        # Display "Bot is thinking..." message while bot generates response
+        with st.spinner(text="Bot is thinking..."):
+            # Update the chat session with the user's input
+            st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
 
-        # Get the chatbot's response from the last message in the history
-        advisor_response = st.session_state.sessionAdvisor.messages[-1]['content'] if st.session_state.sessionAdvisor.messages else ""
+            # Get the chatbot's response from the last message in the history
+            advisor_response = st.session_state.sessionAdvisor.messages[-1]['content'] if st.session_state.sessionAdvisor.messages else ""
 
-        # Remove newlines and extra spaces from the response
-        advisor_response = advisor_response.replace('\n', ' ').strip()
+            # Remove newlines and extra spaces from the response
+            advisor_response = advisor_response.replace('\n', ' ').strip()
 
-        # Add the chatbot's response to the chat history
-        st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
+            # Identify choices based on predefined financial categories
+            identified_choices = []
+            for category in financial_categories:
+                if category in advisor_response.lower():
+                    identified_choices.append(category)
 
-        # Display the chat history including new messages
-        chat_messages = ""
-        if st.session_state.chat_history:
-            for message in st.session_state.chat_history:
-                role_color = "#9400D3" if message["role"] == "user" else "#0084ff"
-                alignment = "left" if message["role"] == "user" else "right"
-                chat_messages += f'<div style="text-align: {alignment}; margin-bottom: 10px;"><span style="background-color: {role_color}; color: white; padding: 8px 12px; border-radius: 20px; display: inline-block; max-width: 70%;">{message["content"]}</span></div>'
-        
-        chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll;">{chat_messages}</div>', unsafe_allow_html=True)
+            # Display buttons for identified choices
+            for choice in identified_choices:
+                if st.button(choice.capitalize()):
+                    # Add the user's choice to the chat history
+                    st.session_state.chat_history.append({"role": "user", "content": choice})
+
+            # Add the bot's response to the chat history
+            st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
+
+        # Display the updated chat history including new messages
+        chat_and_thinking_display = update_chat_display(st.session_state.chat_history) + '<div id="thinking"></div>'
+        chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll; position: relative;">{chat_and_thinking_display}</div>', unsafe_allow_html=True)
 
     # Create a button to start a new conversation
     if st.button("New Chat"):
