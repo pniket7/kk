832f3d2d0673feec7f19b2b54defec148e458059
Niket Patil <pniket7@gmail.com>
2023-12-07 12:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -261,4 +261,4 @@ def main():
         st.markdown("Chatbot session exited. You can start a new conversation by clicking the 'New Chat' button.")
 
 if __name__ == "__main__":
-    main(
+    main()
