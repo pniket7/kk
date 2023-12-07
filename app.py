2cbf2bb3022602f91c6fcef0fcac2ac5934606d6
Niket Patil <pniket7@gmail.com>
2023-12-07 11:55:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,6 +232,9 @@ def main():
             # Add the bot's response to the chat history
             st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
 
+        # Clear the input field after sending the message
+        user_input = ""
+
         # Display the updated chat history including new messages
         chat_and_thinking_display = update_chat_display(st.session_state.chat_history) + '<div id="thinking"></div>'
         chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll; position: relative;">{chat_and_thinking_display}</div>', unsafe_allow_html=True)
