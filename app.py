d8f5c5f946f362e7c7b5cdfdcf871b3c7e0d7fde
Niket Patil <pniket7@gmail.com>
2023-12-07 13:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,6 +241,9 @@ def main():
         chat_and_thinking_display = update_chat_display(st.session_state.chat_history) + '<div id="thinking"></div>'
         chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll; position: relative;">{chat_and_thinking_display}</div>', unsafe_allow_html=True)
 
+        # Clear the input field for the next message
+        st.text_input("Type your message here...", key=input_key, value=user_input)
+
     # Create a button to start a new conversation
     if st.button("New Chat"):
         # Clear the chat history to start a new conversation
